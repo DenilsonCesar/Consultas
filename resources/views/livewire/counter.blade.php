516ef0f7a01f6d5797a3af7e1c7b0f5013b21798
D3NIILSON <contato@sindsystem.com.br>
2023-09-18 21:22:14 -0300

    Inicio do projeto

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div style="text-align: center">
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+
+    <button class="bg-blue-500 md:bg-green-500 ...">Subscribe</button>
+</div>
